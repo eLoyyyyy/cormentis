13b4ed00ace4659b097f79b7d45370fadcd094d4
jem say <jem07.say@gmail.com>
2017-03-08 16:39:31 +0800

    custom-team.php
    Newly Added Custom Team

---
+++ template-parts/custom-team.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Template Name: Our Team
+*
+* @link https://developer.wordpress.org/themes/template-files-section/page-templates/
+*
+* @package cormentis
+*/
+
+get_header();
+?>
+<div class="container">
+	<div id="primary" class="content-area">
+		<main id="our-team" class="site-main" role="main">
+			<?php the_content( ); ?>
+		</main>
+	</div>
+</div>
+<?php get_footer(); ?>
