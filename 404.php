a356562e4e4cbd6edc5f894d68246f6d825b4702
jem say <jem07.say@gmail.com>
2017-03-08 16:36:30 +0800

    Update 404.php
    latest update

--- 404.php
+++ 404.php
@@ -8,57 +8,32 @@
  */
 
 get_header(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
+						<section class="error-404 not-found">
+							<header class="page-header">
+								<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'cormentis' ); ?></h1>
+							</header><!-- .page-header -->
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+							<div class="page-content">
+								<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'cormentis' ); ?></p>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'cormentis' ); ?></h1>
-				</header><!-- .page-header -->
+								<?php get_search_form(); ?>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'cormentis' ); ?></p>
+							</div><!-- .page-content -->
+						</section><!-- .error-404 -->
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( cormentis_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'cormentis' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'cormentis' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					</main><!-- #main -->
+				</div><!-- #primary -->
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+				<?php get_sidebar();?>
+			</div>
+		</div>
+	</div>
 
 <?php
 get_footer();
